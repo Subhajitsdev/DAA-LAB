5b252f990a0f201798c0874a74379ce629ac3b87
Subhajitsdev <120780641+Subhajitsdev@users.noreply.github.com>
2024-03-18 12:06:10 +0530

    Add files via upload

--- zeroknap.c
+++ zeroknap.c
@@ -3,26 +3,37 @@ int max(int a, int b){
 	if(a>b) return a;
 	else  return b;
 }
-int knapsack(int W, int wt[], int profit[], int n) {
-   int i, j;
+int i, j;
+/*int knapsack(int W, int wt[], int profit[], int n) {
+  
+   return B[n][W];
+}*/
+int main() {
+   int profit[] = {1000, 2000, 1600,600};
+   int wt[] = {3, 2, 4,6};
+   int W = 6;
+   int n = sizeof(profit)/sizeof(profit[0]);
+    
    int B[n+1][W+1];
    for (i = 0; i <= n; i++) {
       for (j = 0; j <= W; j++) {
          if (i==0 || j==0)
             B[i][j] = 0;
-         else if (wt[i-1] <= j)
+         else {
+         if (wt[i-1] <= j) {
             B[i][j] = max(profit[i-1] + B[i-1][j-wt[i-1]], B[i-1][j]);
-         else
+            }
+         else{
             B[i][j] = B[i-1][j];
+            }
+         }
       }
+   } 
+   for(i=0;i<=n;i++){
+   	for(j=0;j<=W;j++){
+   		printf("%d\t",B[i][j]);
+   	} printf("\n");
    }
-   return B[n][W];
-}
-int main() {
-   int profit[] = {20, 25, 40};
-   int wt[] = {25, 20, 30};
-   int W = 50;
-   int n = sizeof(profit)/sizeof(profit[0]);
-   printf("The maximum profit is : %d\n", knapsack(W, wt, profit, n));
+   printf("The maximum profit is : %d\n", B[n][W]);
    return 0;
 }
